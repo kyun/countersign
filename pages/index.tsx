7fad0bc8a40801b018d2986fb7e2440c93a99b39
kyun <wls5563@gmail.com>
2022-09-13 01:30:56 +0900

    crawling

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,8 +4,12 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import React from "react";
 import axios from "axios";
+import { parse } from "node-html-parser";
 
-const Home: NextPage = ({ data, date }: any) => {
+const Home: NextPage = ({ date, bbcHead, cnnHead }: any) => {
+  React.useEffect(() => {
+    console.log(bbcHead);
+  }, [bbcHead]);
   return (
     <div className={styles.container}>
       <Head>
@@ -14,24 +18,27 @@ const Home: NextPage = ({ data, date }: any) => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <h1>deployed at : {date}</h1>
-      <h1>{data.info.seed}</h1>
-      <h2>{data.results[0].picture.large}</h2>
-      <Image
-        loader={({ src }) => src}
-        src={data.results[0].picture.large}
-        alt="picture"
-        width={256}
-        height={256}
-      />
+      <h1>{bbcHead}</h1>
+      <h1>{cnnHead}</h1>
     </div>
   );
 };
 
 export const getStaticProps = async () => {
-  const { data } = await axios.get("https://randomuser.me/api/");
+  const { data: bbcHTML } = await axios.get(`https://www.bbc.com/news`);
+
+  const bbcRoot = parse(bbcHTML);
+  const bbcHead = bbcRoot.querySelector(".gs-c-promo-heading__title")?.text;
+
+  const { data: cnnHTML } = await axios.get(`https://www.bbc.com/future`);
+  const cnnRoot = parse(cnnHTML);
+  const cnnHead =
+    cnnRoot.querySelector(".article-hero__title-text")?.text ?? "null";
+
   return {
     props: {
-      data,
+      bbcHead,
+      cnnHead,
       date: new Date(Date.now()).toLocaleString(),
     },
   };
